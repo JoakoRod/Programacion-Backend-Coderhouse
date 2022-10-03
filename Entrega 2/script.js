ca1d013277814638d5b00a3a4ee0c10db274f204
Joaquin <222joaignaciorodriguez222>
2022-10-03 11:26:17 -0300

    primer metodo

--- Entrega 2/script.js
+++ Entrega 2/script.js
@@ -1,6 +1,34 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require("fs");
+const path = require("path");
+
+
+
 
 class Contenedor {
-    
+    constructor(nombreArchivo) {
+        this.archivo = nombreArchivo;
+    }
+
+    async getAll() {
+        try {
+            let resultado = await fs.promises.readFile(`./${this.archivo}`, "utf-8");
+            return JSON.parse(resultado);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    getById(Number){
+        
+    }
 }
+
+
+
+async function main() {
+    const contenedor = new Contenedor("productos.txt");
+    let productos = await contenedor.getAll();
+    console.log(productos);
+}
+
+main();
