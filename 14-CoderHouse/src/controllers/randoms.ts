b64b3a94b2bed3a771da767d1d49fb49047246ac
Joaquin <222joaignaciorodriguez222>
2022-12-28 12:03:37 -0300

    Completo

---
+++ 14-CoderHouse/src/controllers/randoms.ts
@@ -0,0 +1,18 @@
+function listaRandom(cant: number) {
+    const nums: number[] = [];
+    for (let i = 0; i < cant; i++) {
+        nums.push(Math.floor(Math.random() * (1000 - 1 + 1) + 1));
+    }
+    return nums
+}
+
+process.on('message', (data: { msg: string; cant: number; }) => {
+    if (data.msg == 'random') {
+        const sum = listaRandom(data.cant);
+        
+        if (typeof process.send === 'function') { //si no pongo el condicional, TS me tira un error.
+            process.send(sum);
+        }
+    }
+
+});
--- 14-CoderHouse/src/routes/api/randoms.ts
+++ 14-CoderHouse/src/routes/api/randoms.ts
@@ -1,12 +1,18 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import { fork } from 'child_process';
+import path from 'path';
+
+const scriptPath = path.resolve(__dirname, '../../controllers/randoms.ts');
 const router = Router();
 
 router.get('/randoms', (req: Request, res: Response, next: NextFunction) => {
+    const arrayNums = fork(scriptPath);
     const cant = req.query.cant || 1000000; //100.000.000 es mucho
-    const nums: number[] = [];
-    for (let i = 0; i < cant; i++)
-        nums.push(Math.floor(Math.random() * (1000 - 1 + 1) + 1));
-    res.json(nums);
+
+    arrayNums.send({msg: 'random', cant: cant});
+    arrayNums.on('message', array => {
+        res.json(array);
+    })
 })
 
 export default router;
